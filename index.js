2081f82ce5794dbebd921ace3471843169904de5
Joe Dietrich <joe@joedietrich.net>
2021-06-01 22:47:34 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const dodgerOldPosition = parseInt(dodger.style.left.replace('px', ''), 10);
+    const dodgerNewPosition = dodgerOldPosition === 0 ? 0 : dodgerOldPosition - 1;
+    dodger.style.left = dodgerNewPosition + 'px';
+}
+
+function moveDodgerRight() {
+  const dodger = document.getElementById('dodger');
+  const dodgerOldPosition = parseInt(dodger.style.left.replace('px', ''), 10);
+  const dodgerNewPosition = dodgerOldPosition === 360 ? 360 : dodgerOldPosition + 1;
+  dodger.style.left = dodgerNewPosition + 'px';
+
+}
+
+function main() {
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowLeft') moveDodgerLeft();
+    if (e.key === 'ArrowRight') moveDodgerRight();
+  });
+}
+main();
